c210dd97b11c438be3b00e68b69088bb5e2c1147
srwatlanta <shawnrwheeler@gmail.com>
2019-08-05 14:03:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let i = document.getElementById("text");
+    i.textContent = "This is really cool!";
+  });
+   
